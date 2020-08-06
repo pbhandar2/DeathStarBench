f9324f5670da28dbeb72694e0a1caa01d2370770
root <root@wisdom1.fyre.ibm.com>
2020-08-05 17:43:57 -0700

    create a PV and PVC test

--- socialNetwork/openshift/scripts/create-all-micro-services.sh
+++ socialNetwork/openshift/scripts/create-all-micro-services.sh
@@ -5,6 +5,9 @@ NS="social-network"
 
 oc create namespace ${NS}
 
+oc create -f pv.yaml
+oc create -f user-mongo-pvc.yaml
+
 for service in *service*.yaml ; do
   oc apply -f $service -n ${NS}
 done
